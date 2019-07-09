d6914951182ff4976e56130bd284fad2e6664b6f
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2019-07-10 00:01:56 +0900

    Extract Options class

---
+++ EventLogBencher/Optionscs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventLogBencher
+{
+    class Options
+    {
+        [CommandLine.Option('w', "wait-msec", Required = true, HelpText = "ループで待つミリ秒")]
+        public string WaitMSec
+        {
+            get;
+            set;
+        }
+
+        [CommandLine.Option('t', "total-events", Required = true, HelpText = "出力するイベントの総数")]
+        public string TotalEvents
+        {
+            get;
+            set;
+        }
+    }
+}
--- EventLogBencher/Program.cs
+++ EventLogBencher/Program.cs
@@ -6,23 +6,6 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-class Options
-{
-    [CommandLine.Option('w', "wait-msec", Required = true, HelpText = "ループで待つミリ秒")]
-    public string WaitMSec
-    {
-        get;
-        set;
-    }
-
-    [CommandLine.Option('t', "total-events", Required = true, HelpText = "出力するイベントの総数")]
-    public string TotalEvents
-    {
-        get;
-        set;
-    }
-}
-
 namespace EventLogBencher
 {
     class Program
