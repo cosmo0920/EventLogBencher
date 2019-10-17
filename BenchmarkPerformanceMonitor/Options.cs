9abe7d7877e937c8623e846c22f46f637f2ee316
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2019-10-17 12:05:34 +0900

    Add BenchmarkPerformanceMonitor tool

---
+++ BenchmarkPerformanceMonitor/Options.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkPerformanceMonitor
+{
+    class Options
+    {
+        [CommandLine.Option('t', "total-seconds", Required = true, HelpText = "実行秒数")]
+        public string TotalSeconds
+        {
+            get;
+            set;
+        }
+
+        [CommandLine.Option('o', "output", Required = false, Default = "dummy.log", HelpText = "出力するファイルの名前")]
+        public string OutputFile
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ BenchmarkPerformanceMonitor/Program.cs
@@ -0,0 +1,74 @@
+﻿using SharedUtility;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BenchmarkPerformanceMonitor
+{
+    class Program
+    {
+        private static DateTime UNIX_EPOCH = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+
+        static void Main(string[] args)
+        {
+            long loggingSteps = 50;
+
+            CommandLine.ParserResult<Options> result = CommandLine.Parser.Default.ParseArguments<Options>(args);
+
+            if (result.Tag == CommandLine.ParserResultType.Parsed)
+            {
+                var parsed = (CommandLine.Parsed<Options>)result;
+
+                loggingSteps = Convert.ToInt64(parsed.Value.TotalSeconds);
+
+                Console.WriteLine("loggingSteps: {0}", loggingSteps);
+
+                DoMonitor(loggingSteps);
+            }
+            else
+            {
+                Console.WriteLine("\nPlease check correct arguments!");
+            }
+        }
+
+        public static long GetUnixTime(DateTime targetTime)
+        {
+            targetTime = targetTime.ToUniversalTime();
+            TimeSpan elapsedTime = targetTime - UNIX_EPOCH;
+
+            return (long)elapsedTime.TotalSeconds;
+        }
+
+        private static void DoMonitor(long loggingSteps)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            Console.WriteLine("steps\tWorking Set(MB)\tPrivate Memory(MB)\tPage File(MB)\tTotal CPU Usage");
+            TotalCPUCounter counter = new TotalCPUCounter();
+            MonitorProcesses monitor = new MonitorProcesses(counter);
+
+            for (int i = 0; i < loggingSteps; i++)
+            {
+                DateTime targetTime = DateTime.Now;
+                long currentTime = GetUnixTime(targetTime);
+
+                Console.Write(String.Format("{0, 8}", i));
+                Task.Run(() => monitor.Run());
+
+                while (GetUnixTime(DateTime.Now) <= currentTime)
+                {
+                    Thread.Sleep(1);
+                }
+            }
+            sw.Stop();
+            Console.Write(String.Format("{0, 8}", loggingSteps));
+            monitor.Run();
+            Console.WriteLine(String.Format("elapsed time(second): {0}", (float)(sw.ElapsedMilliseconds / 1000.0)));
+
+            Console.WriteLine("Minotoring done.");
+        }
+    }
+}
