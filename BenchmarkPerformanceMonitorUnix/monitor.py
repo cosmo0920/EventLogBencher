923e159b5bf406dbeb51118375644342f4e3fba4
Hiroshi Hatake <hatake@clear-code.com>
2019-10-17 16:38:25 +0900

    Add monitor tool for non-Windows

---
+++ BenchmarkPerformanceMonitorUnix/monitor.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import psutil
+import time
+from datetime import datetime
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("steps", help="Total running seconds",
+                    type=int)
+args = parser.parse_args()
+
+print("steps\tRSS(MB)\tTotal CPU Usage(%)")
+steps = 1
+RUBY = "ruby"
+rss = 0
+
+while steps <= args.steps:
+    currentTime = datetime.now().strftime("%s")
+    for proc in psutil.process_iter():
+        if proc.name() == RUBY:
+            rss = proc.memory_info().rss + rss
+    cpu = psutil.cpu_percent(interval=1)
+    print(f"{steps}\t{rss /1024/1024 :8}\t{cpu:8}")
+    steps = steps + 1
+    rss = 0
+    while (int(currentTime) >= int(datetime.now().strftime("%s"))):
+        time.sleep(0.01)
